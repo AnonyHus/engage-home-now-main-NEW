3c12e4402504d4f1eec090da6b310af6ee5e54ca
Ahmedmemo <anonymous.survs@gmail.com>
2025-11-09 05:56:14 +0200

    Test Commit

--- src/pages/admin/DataSyncPage.tsx
+++ src/pages/admin/DataSyncPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Swal from "sweetalert2";
 import {
-  downloadLocalStorageAsFile,
+  downloadLocalStorageAsFile, 
   uploadLocalStorageFromFile,
   exportLocalStorage,
   importLocalStorage,
